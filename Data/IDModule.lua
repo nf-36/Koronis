ab6770b130447b99382b027a69e822a005c42645
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-01 00:01:03 -0600

    Update IDModule.lua

--- Data/IDModule.lua
+++ Data/IDModule.lua
@@ -7,4 +7,42 @@ module.IDs = {
     [122220249529691] = "10934afa53546c3bbb7e6f8dd76cee72"
 }
 
+module.Functions = {
+    CheckKey = function(args)
+        local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
+
+        api.script_id = args.ScriptID
+    
+        local status = api.check_key(args.Key)
+
+        if status.code == "KEY_VALID" then
+            api.load_script();
+            Script.Fluent:Destroy()
+            script:Destroy()
+            return
+        elseif (status.code == "KEY_HWID_LOCKED") then
+            Script.Fluent.UI:Notify({
+                Title = "Error",
+                Content = "This key is locked to another device. Please contact support.",
+                Duration = 5,
+            })
+            return
+        elseif (status.code == "KEY_INCORRECT") then
+            Script.Fluent.UI:Notify({
+                Title = "Error",
+                Content = "This key is incorrect. Please try again.",
+                Duration = 5,
+            })
+            return   
+        elseif (status.code == "KEY_INVALID") then
+            Script.Fluent.UI:Notify({
+                Title = "Error",
+                Content = "This key is not a valid paradise.cc key.",
+                Duration = 5,
+            })
+            return   
+        end
+    end
+}
+
 return module
