c6e25aaf7cf518284521842185bf33c5c0f238f8
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-01 15:21:47 -0600

    Update KeySystemModule.lua

--- Data/KeySystemModule.lua
+++ Data/KeySystemModule.lua
@@ -1,12 +1,12 @@
 local module = {}
 
-module.Fluent = nil
 module.IDs = {
     ["Universal"] = "baf0792f6cce01ba2040d6bf52996eb8",
     [14350413280] = "dd624dce3675715d34af679cdc1f6860",
     [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288",
     [122220249529691] = "10934afa53546c3bbb7e6f8dd76cee72"
 }
+module.Notify = nil
 module.ScriptID = module.IDs[game.PlaceId] and module.IDs[game.PlaceId] or module.IDs.Universal
 
 module.Functions = {
@@ -18,13 +18,12 @@ module.Functions = {
         local status = api.check_key(Key)
 
         if status.code == "KEY_VALID" then
-            api.load_script();
-            module.Fluent:Destroy()
+            api.load_script()
             script:Destroy()
             return
         else
-            if module.Fluent ~= nil then
-                module.Fluent:Notify({
+            if module.Notify ~= nil then
+                module.Notify({
                     Title = "Error",
                     Content = status.message,
                     Duration = 5,
