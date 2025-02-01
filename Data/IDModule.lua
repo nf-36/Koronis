3411399a3a335094980f3fbced81d284a4d0b059
Robby <90345430+nf-36@users.noreply.github.com>
2025-01-31 23:55:59 -0600

    Create IDModule.lua

---
+++ Data/IDModule.lua
@@ -0,0 +1,10 @@
+local module = {}
+
+module.IDs = {
+    ["Universal"] = "baf0792f6cce01ba2040d6bf52996eb8",
+    [14350413280] = "dd624dce3675715d34af679cdc1f6860",
+    [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288",
+    [122220249529691] = "10934afa53546c3bbb7e6f8dd76cee72"
+}
+
+return module
