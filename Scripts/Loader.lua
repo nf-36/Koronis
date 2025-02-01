8e064d63d695cd872e94317e1c70a6bbde1d9865
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-01 00:08:25 -0600

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -10,52 +10,11 @@ local Script = {
             Acrylic = true, 
         }
     },
-    LoaderScripts = {
-        ["Universal"] = "baf0792f6cce01ba2040d6bf52996eb8",
-        [14350413280] = "dd624dce3675715d34af679cdc1f6860",
-        [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288",
-        [122220249529691] = "10934afa53546c3bbb7e6f8dd76cee72"
-    }
-}
-Script.LoaderFunctions = {
-    CheckKey = function(args)
-        local api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
-
-        api.script_id = args.ScriptID
-    
-        local status = api.check_key(args.Key)
-
-        if status.code == "KEY_VALID" then
-            api.load_script();
-            Script.Fluent:Destroy()
-            script:Destroy()
-            return
-        elseif (status.code == "KEY_HWID_LOCKED") then
-            Script.Fluent.UI:Notify({
-                Title = "Error",
-                Content = "This key is locked to another device. Please contact support.",
-                Duration = 5,
-            })
-            return
-        elseif (status.code == "KEY_INCORRECT") then
-            Script.Fluent.UI:Notify({
-                Title = "Error",
-                Content = "This key is incorrect. Please try again.",
-                Duration = 5,
-            })
-            return   
-        elseif (status.code == "KEY_INVALID") then
-            Script.Fluent.UI:Notify({
-                Title = "Error",
-                Content = "This key is not a valid paradise.cc key.",
-                Duration = 5,
-            })
-            return   
-        end
-    end
+    KeyModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/nf-36/paradise.cc/refs/heads/main/Data/IDModule.lua"))(),
 }
 
 local Window = Script.Fluent.UI:CreateWindow(Script.Fluent.Window)
+Script.KeyModule.Fluent = Script.Fluent.UI
 
 Tabs = {
     Auth = function ()
@@ -72,15 +31,15 @@ Tabs = {
         Authenticate:AddButton({
             Title = "Redeem Key",
             Callback = function()
-                if Script.LoaderScripts[game.PlaceId] then
-                    Script.LoaderFunctions.CheckKey({
+                if Script.KeyModule.IDs[game.PlaceId] then
+                    Script.KeyModule.Functions.CheckKey({
                         Key = script_key,
-                        ScriptID = Script.LoaderScripts[game.PlaceId]
+                        ScriptID = Script.KeyModule.IDs[game.PlaceId]
                     })
                 else
-                    Script.LoaderFunctions.CheckKey({
+                    Script.KeyModule.Functions.CheckKey({
                         Key = script_key,
-                        ScriptID = Script.LoaderScripts.Universal
+                        ScriptID = Script.KeyModule.IDs.Universal
                     })
                 end
             end
