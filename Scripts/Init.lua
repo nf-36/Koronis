2efe952a4d2e25e2b0e7a5474490831d2bffc473
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-14 14:18:37 -0600

    Update Init.lua

--- Scripts/Init.lua
+++ Scripts/Init.lua
@@ -96,8 +96,8 @@ function module.BuildSettings(tab)
 
 
     
-    module.InterfaceManager:SetFolder("Paradise")
-    module.SaveManager:SetFolder("Paradise/".. game.PlaceId)
+    module.InterfaceManager:SetFolder("Koronis")
+    module.SaveManager:SetFolder("Koronis/".. game.PlaceId)
 
     module.InterfaceManager:BuildInterfaceSection(tab)
     module.SaveManager:BuildConfigSection(tab)
