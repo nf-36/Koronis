a3a2e50cf4a231161c8444f7026e2152653a5ffd
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-14 16:27:14 -0600

    Update KeySystemModule.lua

--- Data/KeySystemModule.lua
+++ Data/KeySystemModule.lua
@@ -20,7 +20,6 @@ module.Functions = {
         if status.code == "KEY_VALID" then
             if module.Notify ~= nil then
                 script_key = Key
-                module.Notify("Success",status.message,5)
                 loadstring(game.GetObjects(game, 'rbxassetid://110010566881663')[1].Source)()
             end
             return {STATUS=true,API=api,KEYSCRIPT=script}
