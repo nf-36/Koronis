12906e2384cb99bccc2e43dd22e987563cf8aa05
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-25 22:41:40 -0600

    Update KeySystemModule.lua

--- Data/KeySystemModule.lua
+++ Data/KeySystemModule.lua
@@ -24,7 +24,11 @@ module.IDs = {
     [14350413280] = "dd624dce3675715d34af679cdc1f6860",
     [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288",
     [16981421605] = "d8915bbb7d1b33a9a95deb956eeb5f2c",
-    [8737602449] = "cbb78863ac6a5a39812d96b9f2c680f7"
+    [8737602449] = "cbb78863ac6a5a39812d96b9f2c680f7" -- PLS DONATE
+    [14966088789] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE BUT INF
+    [6652551895] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Donate Game
+    [8916037983] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Artists
+    
 }
 module.Notify = nil
 module.ScriptID = module.IDs[game.PlaceId] and module.IDs[game.PlaceId] or module.IDs.Universal
