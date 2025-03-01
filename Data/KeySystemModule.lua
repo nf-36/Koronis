c48beafea02e3082d558b1072f23f2fba2f55c3b
Robby <90345430+nf-36@users.noreply.github.com>
2025-02-28 21:44:21 -0600

    Update KeySystemModule.lua

--- Data/KeySystemModule.lua
+++ Data/KeySystemModule.lua
@@ -20,15 +20,16 @@ end
 
 
 module.IDs = {
-    ["Universal"] = "baf0792f6cce01ba2040d6bf52996eb8",
-    [14350413280] = "dd624dce3675715d34af679cdc1f6860",
-    [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288",
-    [16981421605] = "d8915bbb7d1b33a9a95deb956eeb5f2c",
-    [8737602449] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE
-    [14966088789] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE BUT INF
-    [6652551895] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Donate Game
-    [8916037983] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Artists
-    [140588718649817] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Pianists
+    ["Universal"] = "baf0792f6cce01ba2040d6bf52996eb8", --  | Koronis Hub
+    [14350413280] = "dd624dce3675715d34af679cdc1f6860", --  | Koronis Hub
+    [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288", --  | Koronis Hub
+    [16981421605] = "d8915bbb7d1b33a9a95deb956eeb5f2c", -- | Koronis Hub
+    [8737602449] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE | DonationAI
+    [14966088789] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE BUT INF | DonationAI
+    [6652551895] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Donate Game | DonationAI
+    [8916037983] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Artists | DonationAI
+    [140588718649817] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Pianists | DonationAI
+    [125723653259639] = "179b8d89aeb59c86b1faac02490982c7" -- Drill Digging Simulator | Koronis Hub
 }
 module.Notify = nil
 module.ScriptID = module.IDs[game.PlaceId] and module.IDs[game.PlaceId] or module.IDs.Universal
