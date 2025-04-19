65728a2f27232982c2b8cd117178ed27a4a42478
Robby <90345430+nf-36@users.noreply.github.com>
2025-04-18 20:59:23 -0500

    Update KeySystemModule.lua

--- Data/KeySystemModule.lua
+++ Data/KeySystemModule.lua
@@ -5,15 +5,11 @@ module.IDs = {
     [14350413280] = "dd624dce3675715d34af679cdc1f6860", --  | Koronis Hub
     [8884433153] = "2f3e1443b79ad9ca4c483dcf537d4288", --  | Koronis Hub
     [16981421605] = "d8915bbb7d1b33a9a95deb956eeb5f2c", -- | Koronis Hub
-    [8737602449] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE | DonationAI
-    [14966088789] = "cbb78863ac6a5a39812d96b9f2c680f7", -- PLS DONATE BUT INF | DonationAI
-    [6652551895] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Donate Game | DonationAI
-    [8916037983] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Artists | DonationAI
-    [140588718649817] = "cbb78863ac6a5a39812d96b9f2c680f7", -- Starving Pianists | DonationAI
     [125723653259639] = "179b8d89aeb59c86b1faac02490982c7", -- Drill Digging Simulator | Koronis Hub,
     [142823291] = "715b720f239e20ee194665e05b77ad6e", -- Murder Mystery 2 | Koronis Hub
     [73801682582529] = "fe3a3d936137408a4f153979276d1416", -- Zombie RNG Lobby | Koronis Hub
-    [84759971088447] = "fe3a3d936137408a4f153979276d1416" -- Zombie RNG Game | Koronis Hub
+    [84759971088447] = "fe3a3d936137408a4f153979276d1416", -- Zombie RNG Game | Koronis Hub
+    [126884695634066] = "c20cd2b9eb243faf8a3884d9ea459bf1" -- Grow A Garden | Koronis Hub
 }
 module.ScriptID = module.IDs[game.PlaceId] and module.IDs[game.PlaceId] or module.IDs.Universal
 module.api = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
