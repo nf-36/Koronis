73e91421bc09a189c138150e0d9ebe0f4aa11e40
Robby <90345430+nf-36@users.noreply.github.com>
2025-06-22 06:49:53 -0500

    Update Loader.lua

--- Scripts/Loader.lua
+++ Scripts/Loader.lua
@@ -501,7 +501,7 @@ local script = G2L["25"];
 			button.Activated:Connect(function()
 				if button.Name == "DISCORD" then
 					if setclipboard then 
-						setclipboard("https://discord.gg/KThfNukpzF")
+						setclipboard("https://discord.gg/koronis")
 					end
 					sendNotification("Koronis has been copied to your clipboard!")
 				elseif button.Name == "SUBMIT" then
